8c95d30424d2a10e19b40a9477e50d4df46213b0
Frederic Conrotte <frederic@conrotte.be>
2016-12-31 17:26:18 +0100

    updates

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 crontab -l > scheduler.cron
-echo "0 * * * * pi /home/pi/meteokit/code/scheduler >> /var/log/meteokit.log" >> scheduler.cron
+echo "0 * * * * /home/pi/meteokit/code/scheduler >> /var/log/meteokit.log" >> scheduler.cron
 crontab scheduler.cron
 rm scheduler.cron
 sudo touch /var/log/meteokit.log
