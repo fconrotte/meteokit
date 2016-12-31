39940fe6cb81efd75211b4c387a4024cb34fb6e6
Frederic Conrotte <frederic@conrotte.be>
2016-12-31 11:13:21 +0100

    updates

--- publish.py
+++ publish.py
@@ -8,8 +8,8 @@ def publishline(row):
    formatted_line = row['datetime'] + "\ntemperature=%2.1f" % row['temperature'] + "°C\npressure=%4.0f" % row['pressure'] + "mb\nhumidity=%2.0f" % row['humidity'] + "%"
    api.update_status(status=formatted_line)
 
-auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-auth.set_access_token(access_token, access_token_secret)
+auth = tweepy.OAuthHandler(config.consumer_key, config.consumer_secret)
+auth.set_access_token(config.access_token, config.access_token_secret)
 
 api = tweepy.API(auth)
 
